cdc905491a7bfaf11beebde88e3cbe78999bdd56
glorious-genre-8033 <namontesino@gmail.com>
2019-08-15 14:46:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -41,4 +41,15 @@ def reduce_to_total(source_array, starting_point = 0)
   end
   starting_point
 end
+
+def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.length
+    if source_array[i] == false
+      return false
+    end
+    i += 1
+  end
+  return true
+end
   
